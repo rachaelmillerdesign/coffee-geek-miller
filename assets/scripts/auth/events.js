aaf53d21ad9145e8fc5211aef1664bf8f57bb1aa
rachael miller design <rachael@rachaelmillerdesign.com>
2018-07-24 09:33:11 -0400

    Making improvements to and simplifying html and css

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const getFormFields = require(`../../../lib/get-form-fields`)
-const game = require('../game.js')
 const api = require('./api')
 const ui = require('./ui')
 
@@ -11,7 +10,7 @@ const ui = require('./ui')
 
 const onSignUp = function (event) {
   event.preventDefault()
-  // console.log('sign up ran!')
+  console.log('sign up ran!')
 
   const data = getFormFields(this)
   api.signUp(data)
@@ -21,7 +20,7 @@ const onSignUp = function (event) {
 
 const onSignIn = function (event) {
   event.preventDefault()
-  // console.log('sign in ran!')
+  console.log('sign in ran!')
 
   const data = getFormFields(this)
   api.signIn(data)
@@ -31,7 +30,7 @@ const onSignIn = function (event) {
 
 const onSignOut = function (event) {
   event.preventDefault()
-  // console.log('sign out ran')
+  console.log('sign out ran')
 
   api.signOut()
     .then(ui.signOutSuccess)
@@ -40,13 +39,22 @@ const onSignOut = function (event) {
 
 const onChangePassword = function (event) {
   event.preventDefault()
-  // console.log('change password ran!')
+  console.log('change password ran!')
 
   const data = getFormFields(this)
   api.changePassword(data)
     .then(ui.changePasswordSuccess)
     .catch(ui.changePasswordFailure)
 }
+
+// ~~~~~~~~~~~~~~~~~~~~~~`
+// HANDLERS
+// ~~~~~~~~~~~~~~~~~~~~~~`
+//
+// const callback = function (e){
+//
+// }
+
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
