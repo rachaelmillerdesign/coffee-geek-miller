11da468d86fba1740e952eaf99c4ecce7ce6004b
rachael miller design <rachael@rachaelmillerdesign.com>
2018-07-26 06:50:24 -0400

    added deployed url into production link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://rachaelmillerdesign.github.io/coffee-geek-miller/',
   development: 'http://localhost:4741'
 }
 
