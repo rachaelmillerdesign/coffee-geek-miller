8bad74f45304894ccb51265ba2daaf8ad0738326
rachael miller design <rachael@rachaelmillerdesign.com>
2018-07-26 08:51:04 -0400

    updated urls for api

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://rachaelmillerdesign.github.io/coffee-geek-miller/',
-  development: 'http://localhost:4741'
+  production: 'https://quiet-fjord-54164.herokuapp.com/',
+  development: 'http://localhost:4741/'
 }
 
 if (window.location.hostname === 'localhost') {
